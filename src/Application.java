8f046e32cbe623d11c1496829af3b02a8e56ea42
ellkka <mihaelatcacib@gmail.com>
2020-09-11 18:17:29 +0300

    ----------

--- src/Application.java
+++ src/Application.java
@@ -1,24 +1,9 @@
-import java.util.Scanner;
-
 public class Application {
-    public static void main(String args []){
-
-        Circle circ = new Circle();
-        Scanner c = new Scanner(System.in);
-        System.out.println("Enter circle radius:");
-        circ.setRadius(c.nextDouble());
-
-       System.out.println("Perimeter is: "+ getPerimeter(circ.getRadius()));
-
-       System.out.println("Area is: " + getArea(circ.getRadius()));
-
-    }
-    static double getArea(double radius){
-        return(Math.PI*radius*radius);
+    public Application() {
     }
 
-    static double getPerimeter(double radius){
-        return(2*Math.PI*radius);
+    public static void main(String[] args) {
+        System.out.println("Perimeter is: " + Circle.getPerimeter(4.0D));
+        System.out.println("Area is: " + Circle.getArea(4.0D));
     }
-
 }
--- src/Circle.java
+++ src/Circle.java
@@ -2,11 +2,11 @@ public class Circle {
 
     private double radius;
 
-    public double getRadius() {
-        return radius;
+    static double getArea(double radius) {
+        return(Math.PI*radius*radius);
     }
 
-    public void setRadius(double radius) {
-        this.radius = radius;
+    static double getPerimeter(double radius) {
+       return(2*Math.PI*radius);
     }
 }
