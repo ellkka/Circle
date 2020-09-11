ada04bbbd5b0e929896284ea9ef824a86d4132fd
ellkka <mihaelatcacib@gmail.com>
2020-09-11 17:12:57 +0300

    Initial commit

---
+++ src/Application.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Application {
+    public static void main(String args []){
+
+        Circle circ = new Circle();
+
+       System.out.println("Perimeter is: "+ circ.getPerimeter(3));
+
+       System.out.println("Area is: "+ circ.getArea(3));
+
+    }
+
+}
---
+++ src/Circle.java
@@ -0,0 +1,13 @@
+public class Circle {
+
+    private double radius;
+
+    public double getArea(double radius){
+        return(Math.PI*radius*radius);
+    }
+
+    public double getPerimeter(double radius){
+        return(radius*radius);
+    }
+
+}
